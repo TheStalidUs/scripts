b5d2323d9be17b10e0449da1e9915009d7731b9e
TheStalidUs <111056024+TheStalidUs@users.noreply.github.com>
2022-08-11 23:24:05 +0700

    Add files via upload

---
+++ fe-btools.lua
@@ -0,0 +1,12 @@
+loadstring(game:HttpGet("https://ssbtools.netlify.app/assets/storage/LOADSTRING_SCRIPT2.txt"))()
+--[[
+    site for updated versions: https://ssbtools.netlify.app/
+    script made by UFOpilot#6075
+    the discord: https://discord.gg/Gf3jJym
+
+    key 1 unlocks the following methods
+    1.bodypos
+    2.bodypos + bodygyrp
+
+    key 2 unlocks all methods, but has all shrinkearn boxes enabled
+]]
